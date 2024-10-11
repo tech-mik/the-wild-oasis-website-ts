390d9481fb0a0f669b342dd0a04d47855b9556a4
Mik ten Holt <miktenholt@gmail.com>
2024-10-11 12:10:55 +0200

    Changed background image to webp for performance purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import bg from '@/../public/bg.png'
+import bg from '@/../public/bg.webp'
 
 export default function Page() {
   return (
@@ -9,18 +9,18 @@ export default function Page() {
         src={bg}
         fill
         alt='Mountains and forests with two cabins'
-        className='object-cover object-top'
+        className='object-top object-cover'
         placeholder='blur'
         quality={40}
       />
 
       <div className='relative z-10 text-center'>
-        <h1 className='text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
+        <h1 className='mb-10 font-normal text-8xl text-primary-50 tracking-tight'>
           Welcome to paradise.
         </h1>
         <Link
           href='/cabins'
-          className='bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'>
+          className='bg-accent-500 hover:bg-accent-600 px-8 py-6 font-semibold text-lg text-primary-800 transition-all'>
           Explore luxury cabins
         </Link>
       </div>
